1d04d362bc35105f2313f09dbbcdd86ba7269ad1
Miguel Angel <miguelangel@peertransfer.com>
2015-10-21 19:08:01 +0000

    select payrolls red

---
+++ specification_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+
+class SpecificationTest < Minitest::Test
+
+  def test_kk
+
+    payroll = "1.000,00,00;EUR;01/03/2015;payroll march"
+    another_payroll = "1.000,00;EUR;02/04/2015;payroll april"
+    transaction = "15.00,00;EUR;12/03/2015;prepaid topup"
+    statement = [payroll, another_payroll, transaction]
+
+    selected_transactions = Account.new(statement).payrolls
+    assert_equal([payroll, another_payroll], selected_transactions)
+  end
+end
+
+class Account
+  def initialize(statement)
+  end
+
+  def payrolls
+    []
+  end
+end
